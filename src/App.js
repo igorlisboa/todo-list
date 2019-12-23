2c244d4b4b0f2f2291743c675bd1f911e70cbae3
Nickael <qaznickael@hotmail.com>
2019-12-23 14:36:15 -0300

    primeira refatoraçao

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import Header from './header/header';
+import './styles.css'
+import Header from './Components/Header/index.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header ></Header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+
+export default function Header() {
+
+    return (
+        <div className="header">
+            <div className="titulo-header">
+                <div>icone</div>
+                <div>nome da pagina</div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
