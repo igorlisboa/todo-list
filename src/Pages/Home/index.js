31dede4120e1094df7ee6feabd6df78e355dbfec
Nickael <qaznickael@hotmail.com>
2019-12-23 22:54:23 -0300

    estilizacoes

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -16,7 +16,25 @@ export default function Home() {
                     <Card title={"Categoria C"} />
                 </div>
                 <div className="adicionar">
-
+                    <div style={{ textAlign: 'center', fontSize: '4vh' }}>
+                        <p>ToDO</p>
+                    </div>
+                    <div>
+                        <div className="opcoes">
+                            <p>titulo</p>
+                            <p>categoria</p>
+                        </div>
+                        <div className="opcoes">
+                            <p>data</p>
+                            <p>anexo</p>
+                        </div>
+                        <div className="opcoes">
+                            <p>descriçao</p>
+                        </div>
+                    </div>
+                    <div className="areaBotao">
+                        <button className="botao">salvar</button>
+                    </div>
                 </div>
             </div>
         </>
